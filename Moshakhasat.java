5566a27a4fca9e5b203d0a719d673d894dac9239
ghafari <azadeh.ghafarinezhad@yahoo.com>
2019-03-11 22:23:16 +0330

    Create Moshakhasat.java

---
+++ Moshakhasat.java
@@ -0,0 +1,11 @@
+package school;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Moshakhasat {
+    String name;
+    String family;
+    ArrayList<String> course = new ArrayList<String>();
+    HashMap<String, Integer> courseNum = new HashMap<String, Integer>();
+}
